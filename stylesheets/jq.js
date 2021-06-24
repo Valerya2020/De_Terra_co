b368b6de2b4fd2de3671fae691b50068da76d3e8
Valerya2020 <svr2014@mail.ru>
2021-06-24 20:31:26 +0300

    main

--- stylesheets/jq.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $(".fito_icon").click(function(){
-  $(this).toggleClass("fito_icon_sun");
-  $(".image-fitolab-card1").toggleClass("image-fitolab-card2");
-  });
-
-
-  });
