c8028c66ea1f8991b03fa0f8874e3b5eb9ed39e7
Valerya2020 <svr2014@mail.ru>
2021-06-24 17:12:37 +0300

    main

--- jq/jq.js
+++ jq/jq.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
   $(".fito_icon").click(function(){
   $(this).toggleClass("fito_icon_sun");
   $(".image-fitolab-card1").toggleClass("image-fitolab-card2");
+  $(".image-fitolab-card3").toggleClass("image-fitolab-card4");
   });
 
   $( function() {
@@ -14,15 +15,25 @@ $(document).ready(function(){
         $(".image-fitolab-card3").removeClass("image-fitolab-card3");
         $(".image-fitolab-card4").addClass("image-fitolab-card2");
         $(".image-fitolab-card4").removeClass("image-fitolab-card4");
+        $(".fito_circle_slider").toggleClass("active_circle");
+        $(".name-fito-card p").toggleClass("hidden_element");
       }
       if (fito_counter == 2) {
           $(".image-fitolab-card1").addClass("image-fitolab-card3");
           $(".image-fitolab-card1").removeClass("image-fitolab-card1");
           $(".image-fitolab-card2").addClass("image-fitolab-card4");
           $(".image-fitolab-card2").removeClass("image-fitolab-card2");
+          $(".fito_circle_slider").toggleClass("active_circle");
+          $(".name-fito-card p").toggleClass("hidden_element");
         }
         if (fito_counter >= 3) {
-          let fito_counter = 0;
+          fito_counter = 1;
+          $(".image-fitolab-card3").addClass("image-fitolab-card1");
+          $(".image-fitolab-card3").removeClass("image-fitolab-card3");
+          $(".image-fitolab-card4").addClass("image-fitolab-card2");
+          $(".image-fitolab-card4").removeClass("image-fitolab-card4");
+          $(".fito_circle_slider").toggleClass("active_circle");
+          $(".name-fito-card p").toggleClass("hidden_element");
           }
     });
   });
